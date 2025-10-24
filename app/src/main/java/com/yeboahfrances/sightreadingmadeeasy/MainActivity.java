ae8136a3d091fc2e6b32cf8667bcb7a82aa8206f
2027fyeboah <francescaddel@gmail.com>
2025-10-24 15:07:27 -0400

    oct 24-quiz functionality, add sound & dropdown

--- app/src/main/java/com/yeboahfrances/sightreadingmadeeasy/MainActivity.java
+++ app/src/main/java/com/yeboahfrances/sightreadingmadeeasy/MainActivity.java
@@ -1,6 +1,8 @@
 package com.yeboahfrances.sightreadingmadeeasy;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -12,5 +14,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        //Fragment firstFragment = new About();
+        Fragment about = new About();
+        Fragment alto = new AltoClef();
+        Fragment bass = new BassClef();
+        Fragment home = new Home();
+        Fragment treble = new TrebleClef();
+
+        setCurrentFragment(home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if(itemId == R.id.about_nav){
+                setCurrentFragment(about);
+            }
+            else if(itemId == R.id.alto_nav){
+                setCurrentFragment(alto);
+            }
+            else if(itemId == R.id.bass_nav){
+                setCurrentFragment(bass);
+            }
+            else if(itemId == R.id.home_nav){
+                setCurrentFragment(home);
+                Button surprise_button = findViewById(R.id.surprise_button);
+                //on button click change to surprise view
+                //call surpriseButton()
+            }
+            else if(itemId == R.id.treble_nav){
+                setCurrentFragment(treble);
+            }
+            return true;
+
+        });
+
+    }
+
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    private void surpriseButton(){
+
     }
 }
--- app/src/main/java/com/yeboahfrances/sightreadingmadeeasy/TrebleClef.java
+++ app/src/main/java/com/yeboahfrances/sightreadingmadeeasy/TrebleClef.java
@@ -1,18 +1,63 @@
 package com.yeboahfrances.sightreadingmadeeasy;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.HapticFeedbackConstants;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.os.Vibrator;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 public class TrebleClef extends Fragment {
-
+    private int[] notes = {R.drawable.treble_staff,R.drawable.treble_staff__2_,R.drawable.treble_staff__3_,R.drawable.treble_staff__4_,R.drawable.treble_staff__5_,R.drawable.treble_staff__6_,R.drawable.treble_staff__7_,R.drawable.treble_staff__8_,R.drawable.treble_staff__9_,R.drawable.treble_staff__10_,R.drawable.treble_staff__11_};
+    private String[] note_names = {"D","E","F","G","A","B","C","D","E","F","G"};
+    int count = 1;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_treble_clef, container, false);
+        View view = inflater.inflate(R.layout.fragment_treble_clef, container, false);
+        ImageView imgs = view.findViewById(R.id.note_imgs);
+        imgs.setImageDrawable(getResources().getDrawable(notes[0]));
+        Button next = view.findViewById(R.id.next);
+        Button submit = view.findViewById(R.id.submit);
+        EditText editText = view.findViewById(R.id.answer);
+        TextView correct = view.findViewById(R.id.correct);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.R)
+            @Override
+            public void onClick(View v) {
+                String answer = editText.getText().toString();
+                if(answer.toUpperCase().equals(note_names[count-1])) {
+                    correct.setText("✨✨Correct!✨✨");
+                    correct.setTextSize(20);
+                    //correct.setTextColor();
+                }
+                else{
+                    correct.setText("Incorrect!");
+                    correct.setTextSize(20);
+                    view.performHapticFeedback(HapticFeedbackConstants.REJECT);
+                }
+            }
+        });
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(count >= notes.length) count %= notes.length;
+                Drawable d = getResources().getDrawable(notes[count]);
+                imgs.setImageDrawable(d);
+                count++;
+            }
+        }); //IT WORKS LESGO just change the size
+        return view;
     }
+
 }
