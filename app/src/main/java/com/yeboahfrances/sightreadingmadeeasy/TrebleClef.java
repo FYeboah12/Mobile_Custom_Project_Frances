70e01e1590f1fb70257467d60f79cc22054f61ef
2027fyeboah <francescaddel@gmail.com>
2025-10-27 15:59:50 -0400

    oct 27- sound implemented, add for dropdown

--- app/src/main/java/com/yeboahfrances/sightreadingmadeeasy/TrebleClef.java
+++ app/src/main/java/com/yeboahfrances/sightreadingmadeeasy/TrebleClef.java
@@ -1,17 +1,20 @@
 package com.yeboahfrances.sightreadingmadeeasy;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.HapticFeedbackConstants;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.os.Vibrator;
+import android.widget.Spinner;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -23,27 +26,36 @@ public class TrebleClef extends Fragment {
     int count = 1;
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        //use view.getContext() instead of this
         View view = inflater.inflate(R.layout.fragment_treble_clef, container, false);
         ImageView imgs = view.findViewById(R.id.note_imgs);
         imgs.setImageDrawable(getResources().getDrawable(notes[0]));
         Button next = view.findViewById(R.id.next);
         Button submit = view.findViewById(R.id.submit);
         EditText editText = view.findViewById(R.id.answer);
-        TextView correct = view.findViewById(R.id.correct);
+        TextView response = view.findViewById(R.id.correct);
+        MediaPlayer mediaPlayer = MediaPlayer.create(view.getContext(), R.raw.note);
+        Spinner dropdown = view.findViewById(R.id.inst_dropdown);
+        String[] instruments = new String[]{"Piano","Clarinet","Bass Clarinet", "Trumpet", "Saxophone", "Violin"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, instruments);
+        dropdown.setAdapter(adapter);
         submit.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.R)
             @Override
             public void onClick(View v) {
                 String answer = editText.getText().toString();
                 if(answer.toUpperCase().equals(note_names[count-1])) {
-                    correct.setText("✨✨Correct!✨✨");
-                    correct.setTextSize(20);
-                    //correct.setTextColor();
+                    response.setText("✨✨Correct!✨✨");
+                    response.setTextSize(20);
+                    response.setTextColor(getResources().getColor(R.color.correct));
+                    mediaPlayer.start(); // this plays piano; get all the notes
+                    //show instrument
                 }
                 else{
-                    correct.setText("Incorrect!");
-                    correct.setTextSize(20);
+                    response.setText("Incorrect!");
+                    response.setTextSize(20);
+                    response.setTextColor(getResources().getColor(R.color.incorrect));
                     view.performHapticFeedback(HapticFeedbackConstants.REJECT);
                 }
             }
@@ -57,6 +69,9 @@ public class TrebleClef extends Fragment {
                 count++;
             }
         }); //IT WORKS LESGO just change the size
+
+        // dropdown.setOnItemSelectedListener(view.getContext());
+
         return view;
     }
 
